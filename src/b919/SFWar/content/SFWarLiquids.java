055c13a3fdc453267cd1cc9fa1a20998a8d2b23a
SentinelDart919 <sentineldart5@gmail.com>
2026-06-22 18:50:57 +0200

    liquids update

--- src/b919/SFWar/content/SFWarLiquids.java
+++ src/b919/SFWar/content/SFWarLiquids.java
@@ -1,6 +1,7 @@
 package b919.SFWar.content;
 
 import arc.graphics.Color;
+import mindustry.content.StatusEffects;
 import mindustry.type.Liquid;
 
 public class SFWarLiquids {
@@ -10,10 +11,29 @@ public class SFWarLiquids {
     public static void load(){
         blood = new Liquid("blood", Color.valueOf("000000")){{}};
         liquidNyctar = new Liquid("liquid-nyctar", Color.valueOf("7d00ab")){{
-
+            temperature = 0.1f;
+            flammability = 0;
+            heatCapacity = 0.7f;
+            explosiveness = 0.4f;
+            viscosity = 0.4f;
+            boilPoint = 4f;
+            blockReactive = false;
+            coolant = true;
+            moveThroughBlocks = false;
+            incinerable = false;
+            effect = StatusEffects.electrified;
         }};
         gaseousNyctar = new Liquid("gaseous-nyctar", Color.valueOf("ab1ae0")){{
             gas = true;
+            temperature = 0.3f;
+            flammability = 0;
+            heatCapacity = 0.2f;
+            explosiveness = 0.2f;
+            viscosity = 0.1f;
+            blockReactive = false;
+            coolant = false;
+            moveThroughBlocks = false;
+            incinerable = false;
         }};
     }
 }

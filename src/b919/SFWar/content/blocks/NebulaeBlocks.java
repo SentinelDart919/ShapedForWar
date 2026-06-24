13b335089beb17f611d549ae9728eae0c4424543
Kravanthana <hmm.floor.67@gmail.com>
2026-06-24 21:40:15 +0300

    New Star Screech turret test

--- src/b919/SFWar/content/blocks/NebulaeBlocks.java
+++ src/b919/SFWar/content/blocks/NebulaeBlocks.java
@@ -1,12 +1,13 @@
 package b919.SFWar.content.blocks;
 
+import arc.graphics.Color;
 import b919.SFWar.world.terran.nebulae.blocks.power.NebulaePanel;
-import mindustry.content.Fx;
-import mindustry.content.Items;
-import mindustry.content.Liquids;
+import mindustry.content.*;
 import mindustry.entities.UnitSorts;
 import mindustry.entities.bullet.*;
+import mindustry.entities.pattern.ShootBarrel;
 import mindustry.gen.Sounds;
+import mindustry.graphics.Pal;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.defense.ForceProjector;
@@ -16,7 +17,6 @@ import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.meta.BuildVisibility;
 import mindustry.type.*; // this will import the classes in this package but not the classes in subpackages
 import mindustry.type.unit.*; // this is not for units this is to set entities, not units
-import mindustry.content.UnitTypes; //this is for units
 import b919.SFWar.content.units.NebulaeUnits;
 import b919.SFWar.content.SFWarItems;
 import b919.SFWar.content.SFWarLiquids;
@@ -34,7 +34,7 @@ public class NebulaeBlocks {
             // defensive
         luminosityCondenser,
             // turrets
-        crescentMoon, heavenPiercer, starScreech, totality,
+        crescentMoon, heavenPiercer, starScreech, pathOfTotality,
             // units
         seminarumStellarum, sartrixNyctar,
             // power
@@ -42,16 +42,17 @@ public class NebulaeBlocks {
     public static void load(){
 
         domusLucis = new CoreBlock("domus-lucis"){{
-            requirements(Category.effect, BuildVisibility.shown, with(SFWarItems.solidifiedNyctar, 1));
+            requirements(Category.effect, BuildVisibility.shown, with(SFWarItems.solidifiedNyctar, 1000));
             health = 10800;
             itemCapacity = 17000;
             size = 6;
 
             unitCapModifier = 32;
             unitType = UnitTypes.toxopid;
+            // toxopid placeholder, replace with Nebulae core unit when possible
         }};
         castellumLucis = new CoreBlock("castellum-lucis"){{
-            requirements(Category.effect, BuildVisibility.shown, with());
+            requirements(Category.effect, BuildVisibility.shown, with(SFWarItems.solidifiedNyctar, 5000, SFWarItems.nyctoSteel, 1000));
             health = 36750;
             itemCapacity = 21000;
             size = 7;
@@ -160,12 +161,76 @@ public class NebulaeBlocks {
 
             coolantMultiplier = 0.6f;
             liquidCapacity = 60f;
-            scaledHealth = 150;
+            scaledHealth = 350;
 
             coolant = consumeCoolant(1f);
             depositCooldown = 2.0f;
             consumePower(10f);
         }};
+        starScreech = new ItemTurret("star-screech"){{
+            requirements(Category.turret, with(Items.graphite, 350, SFWarItems.solidifiedNyctar, 625, SFWarItems.nyctoSteel, 250, Items.silicon, 300));
+            ammo(
+                    SFWarItems.solidifiedNyctar, new MissileBulletType(4.2f, 42){{
+                        width = 12f;
+                        height = 12f;
+                        shrinkY = 0f;
+                        homingPower = 0.25f;
+                        damage = 25;
+                        splashDamageRadius = 35f;
+                        splashDamage = 85f * 1.4f;
+                        hitEffect = Fx.blastExplosion;
+                        despawnEffect = Fx.blastExplosion;
+                        ammoMultiplier = 4f;
+                        status = StatusEffects.tarred;
+
+                        hitColor = backColor = trailColor = Color.valueOf("ab1ae0");
+                        frontColor = Color.valueOf("8e00c2");
+                    }},
+                    SFWarItems.nyctoSteel, new MissileBulletType(4.2f, 42){{
+                        width = 8f;
+                        height = 8f;
+                        shrinkY = 0f;
+                        homingPower = 0.10f;
+                        damage = 125;
+                        splashDamageRadius = 35f;
+                        splashDamage = 10f * 1.4f;
+                        hitEffect = Fx.blastExplosion;
+                        despawnEffect = Fx.blastExplosion;
+                        ammoMultiplier = 4f;
+                        status = StatusEffects.slow;
+
+                        hitColor = backColor = trailColor = Color.valueOf("8e00c2");
+                        frontColor = Color.valueOf("ab1ae0");
+                    }}
+            );
+
+            shoot = new ShootBarrel(){{
+                barrels = new float[]{
+                        -8, -2.5f, 0,
+                        -4, -1.25f, 0,
+                        0, 0, 0,
+                        4, -1.25f, 0,
+                        8, -25.f, 0
+                };
+                shots = 5;
+                shotDelay = 5f;
+            }};
+
+            shootY = 4.5f;
+            reload = 60f * 4f / 7f;
+            inaccuracy = 5f;
+            range = 360f;
+            consumeAmmoOnce = false;
+            size = 4;
+            scaledHealth = 480;
+            shootSound = Sounds.shootMissile;
+            envEnabled |= Env.space;
+
+            limitRange(5f);
+            coolant = consumeCoolant(0.3f);
+            depositCooldown = 2.0f;
+        }};
+
 
     }
 }

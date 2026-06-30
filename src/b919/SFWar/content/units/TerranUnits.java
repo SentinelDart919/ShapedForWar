6b2b77ea22c92df2fd38bc3738a8f8bf4964e8fa
SentinelDart919 <sentineldart5@gmail.com>
2026-06-30 19:34:57 +0200

    example unit

--- src/b919/SFWar/content/units/TerranUnits.java
+++ src/b919/SFWar/content/units/TerranUnits.java
@@ -1,4 +1,84 @@
 package b919.SFWar.content.units;
 
+import mindustry.entities.bullet.BulletType;
+import mindustry.gen.Unitc;
+import mindustry.graphics.Pal;
+import mindustry.type.UnitType;
+import mindustry.type.weapons.RepairBeamWeapon;
+
+import static mindustry.Vars.tilesize;
+
 public class TerranUnits {
+    public static UnitType administrator;
+    public static void load(){
+        administrator = new UnitType("administrator"){{
+            coreUnitDock = true;
+            //controller = u -> new BuilderAI(true, coreFleeRange); - I commented this out, is how the unit is controlled by AI
+            isEnemy = false;
+            envDisabled = 0;
+
+            range = 65f;
+            faceTarget = true;
+            targetPriority = -2;
+            lowAltitude = false;
+            mineWalls = true;
+            mineFloor = false;
+            mineHardnessScaling = false;
+            flying = true;
+            mineSpeed = 9f;
+            mineTier = 3;
+            buildSpeed = 1.5f;
+            drag = 0.08f;
+            speed = 7.5f;
+            rotateSpeed = 8f;
+            accel = 0.08f;
+            itemCapacity = 110;
+            health = 700f;
+            armor = 3f;
+            hitSize = 12f;
+            buildBeamOffset = 8f;
+            payloadCapacity = 2f * 2f * tilesize * tilesize;
+            pickupUnits = false;
+            vulnerableWithPayloads = true;
+
+            fogRadius = 0f;
+            targetable = false;
+            hittable = false;
+
+            engineOffset = 7.5f;
+            engineSize = 3.4f;
+
+            setEnginesMirror(
+                    new UnitEngine(35 / 4f, -13 / 4f, 2.7f, 315f),
+                    new UnitEngine(28 / 4f, -35 / 4f, 2.7f, 315f)
+            );
+
+            weapons.add(new RepairBeamWeapon(){{
+                widthSinMag = 0.11f;
+                reload = 20f;
+                x = 19f/4f;
+                y = 19f/4f;
+                rotate = false;
+                shootY = 0f;
+                beamWidth = 0.7f;
+                aimDst = 0f;
+                shootCone = 40f;
+                mirror = true;
+
+                repairSpeed = 3.6f / 2f;
+                fractionRepairSpeed = 0.03f;
+
+                targetUnits = false;
+                targetBuildings = true;
+                autoTarget = false;
+                controllable = true;
+                laserColor = Pal.accent;
+                healColor = Pal.accent;
+
+                bullet = new BulletType(){{
+                    maxRange = 65f;
+                }};
+            }});
+        }};
+    }
 }

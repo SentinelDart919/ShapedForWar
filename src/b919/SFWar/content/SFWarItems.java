9af23a314158b27f0698bfb6eeb53162d79876df
SentinelDart919 <sentineldart5@gmail.com>
2026-07-09 11:50:18 +0200

    Item update

--- src/b919/SFWar/content/SFWarItems.java
+++ src/b919/SFWar/content/SFWarItems.java
@@ -22,35 +22,82 @@ public class SFWarItems {
             heavenPiercingShell;
     public static void load(){
         uraninite = new Item("uraninite", Color.valueOf("77897a")){{//Ore
+            hardness = 4;
             radioactivity = 0.05f;
+            cost = 2.2f;
         }};
         uranium = new Item("uranium", Color.valueOf("369e46")){{//U238
-            radioactivity = 0.4f;
+            hardness = 4;
+            radioactivity = 0.15f;
+            cost = 2.5f;
         }};
         enrichedUranium = new Item("enriched-uranium", Color.valueOf("000000")){{//U235
+            hardness = 4;
             radioactivity = 1.4f;
         }};
-        ruby = new Item("ruby", Color.valueOf("000000")){{}};
-        ferrum = new Item("ferrum", Color.valueOf("000000")){{}};
-        steel = new Item("steel", Color.valueOf("000000")){{}};
-        chromium = new Item("chromium", Color.valueOf("000000")){{}};
-        sulfur = new Item("sulfur", Color.valueOf("000000")){{}};
-        gunpowder = new Item("gunpowder", Color.valueOf("000000")){{}};
+        ruby = new Item("ruby", Color.valueOf("000000")){{
+            hardness = 2;
+        }};
+        ferrum = new Item("ferrum", Color.valueOf("000000")){{
+            hardness = 1;
+        }};
+        steel = new Item("steel", Color.valueOf("000000")){{
+            hardness = 2;
+            cost = 1.2f;
+        }};
+        chromium = new Item("chromium", Color.valueOf("000000")){{
+            hardness = 3;
+            cost = 1.4f;
+        }};
+        sulfur = new Item("sulfur", Color.valueOf("000000")){{
+            hardness = 2;
+        }};
+        gunpowder = new Item("gunpowder", Color.valueOf("000000")){{
+            explosiveness = 0.9876f;
+            flammability = 0.7f;
+        }};
         foodRations = new Item("food-rations", Color.valueOf("000000")){{}};
-        heavyAlloy = new Item("heavy-alloy", Color.valueOf("000000")){{}};
-        armorPlates = new Item("armor-plates", Color.valueOf("000000")){{}};
-        heavyArmorPlates = new Item("heavy-armor-plates", Color.valueOf("000000")){{}};
-        uraniumPlates = new Item("uranium-plates", Color.valueOf("000000")){{}};
+        heavyAlloy = new Item("heavy-alloy", Color.valueOf("000000")){{
+            cost = 1.5f;
+        }};
+        armorPlates = new Item("armor-plates", Color.valueOf("000000")){{
+            cost = 1.2f;
+        }};
+        heavyArmorPlates = new Item("heavy-armor-plates", Color.valueOf("000000")){{
+            cost = 1.4f;
+        }};
+        uraniumPlates = new Item("uranium-plates", Color.valueOf("000000")){{
+            cost = 2.7f;
+        }};
         smallCase = new Item("small-case", Color.valueOf("000000")){{}};
         mediumCase = new Item("medium-case", Color.valueOf("000000")){{}};
         bigCase = new Item("big-case", Color.valueOf("000000")){{}};
-        commonAmmo = new Item("common-ammo", Color.valueOf("000000")){{}};
-        HEAmmo = new Item("HE-ammo", Color.valueOf("000000")){{}};
-        APAmmo = new Item("AP-ammo", Color.valueOf("000000")){{}};
-        ArtilleryRounds = new Item("artillery-rounds", Color.valueOf("000000")){{}};
-        uraniumAmmo = new Item("uranium-ammo", Color.valueOf("000000")){{}};
-        nuclearFuel = new Item("nuclear-fuel", Color.valueOf("000000")){{}};
-        depletedNuclearFuel = new Item("depleted-nuclear-fuel", Color.valueOf("000000")){{}};
+        commonAmmo = new Item("common-ammo", Color.valueOf("000000")){{
+            flammability = 0.8f;
+            explosiveness = 0.2f;
+        }};
+        HEAmmo = new Item("HE-ammo", Color.valueOf("000000")){{
+            flammability = 0.95f;
+            explosiveness = 1.8f;
+        }};
+        APAmmo = new Item("AP-ammo", Color.valueOf("000000")){{
+            flammability = 0.95f;
+            explosiveness = 0.85f;
+        }};
+        ArtilleryRounds = new Item("artillery-rounds", Color.valueOf("000000")){{
+            flammability = 1.9f;
+            explosiveness = 1.95f;
+        }};
+        uraniumAmmo = new Item("uranium-ammo", Color.valueOf("000000")){{
+            flammability = 0.95f;
+            explosiveness = 1.15f;
+        }};
+        nuclearFuel = new Item("nuclear-fuel", Color.valueOf("000000")){{
+            radioactivity = 2.5f;
+        }};
+        depletedNuclearFuel = new Item("depleted-nuclear-fuel", Color.valueOf("000000")){{
+            radioactivity = 0.25f;
+        }};
         //biomass
         flesh = new Item("flesh", Color.valueOf("000000")){{}};
         denseTissues = new Item("dense-tissues", Color.valueOf("000000")){{}};
